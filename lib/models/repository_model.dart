ecaf385bcf5ea89173800b2201f4caebf4cd2a98
Ansinarose <iansinarose@gmail.com>
2024-09-05 12:04:24 +0530

    model class created

---
+++ lib/models/repository_model.dart
@@ -0,0 +1,35 @@
+class Repository {
+  final String name;
+  final String description;
+  final int stars;
+  final String ownerUsername;
+  final String ownerAvatarUrl;
+
+  Repository({
+    required this.name,
+    required this.description,
+    required this.stars,
+    required this.ownerUsername,
+    required this.ownerAvatarUrl,
+  });
+
+  factory Repository.fromJson(Map<String, dynamic> json) {
+    return Repository(
+      name: json['name'],
+      description: json['description'] ?? '',
+      stars: json['stargazers_count'],
+      ownerUsername: json['owner']['login'],
+      ownerAvatarUrl: json['owner']['avatar_url'],
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'name': name,
+      'description': description,
+      'stars': stars,
+      'ownerUsername': ownerUsername,
+      'ownerAvatarUrl': ownerAvatarUrl,
+    };
+  }
+}
